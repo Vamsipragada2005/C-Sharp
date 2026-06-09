8b0879dbdda55500b0fcd7aa4a176e55f7c10fba
PRAGADA VAMSI <vamsipragada1750@gmail.com>
2026-06-09 13:19:23 +0530

    Update assembly informational versions and cache files across multiple projects

--- DataTypes/obj/Debug/net10.0/DataTypes.AssemblyInfo.cs
+++ DataTypes/obj/Debug/net10.0/DataTypes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataTypes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d09198d547cc8d471031a0d2a1a42f45bf2c7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec1cf6466914cf8f0579edf675c2fb765fafd545")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataTypes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataTypes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
